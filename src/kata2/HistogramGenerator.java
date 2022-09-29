53e68f646aec857eaf100d16675a58ca8348b11c
Entrar <Entrar@10.22.147.55>
2022-09-29 12:33:33 +0100

    Kata2 version 3

---
+++ src/kata2/HistogramGenerator.java
@@ -0,0 +1,22 @@
+package kata2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HistogramGenerator {
+    private final int [] data;
+    
+    public HistogramGenerator(int[] dt){
+        this.data = dt;      
+    }
+    
+    Map<Integer, Integer> getHistogram(){
+        Map<Integer, Integer> histogram = new HashMap<Integer, Integer>();
+        
+        for (int i = 0; i < data.length; i++){
+            histogram.put(data[i], histogram.containsKey(data[i]) ? histogram.get(data[i]) + 1 : 1);
+        }
+        
+        return histogram;
+    }
+}
--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -8,11 +8,9 @@ public class Kata2 {
 
     public static void main(String[] args) {
         int [] data = {4,2,2,4,9,8,0,1,2,6,3,7,8,5};
-        Map<Integer, Integer> histogram = new HashMap<Integer, Integer>();
         
-        for (int i = 0; i < data.length; i++){
-            histogram.put(data[i], histogram.containsKey(data[i]) ? histogram.get(data[i]) + 1 : 1);
-        }        
+        HistogramGenerator histogramGenerator = new HistogramGenerator(data);
+        Map<Integer, Integer> histogram = histogramGenerator.getHistogram();  
         
         for (Map.Entry<Integer, Integer> entry : histogram.entrySet()) {
             System.out.println(entry.getKey() + "==>" + entry.getValue());
